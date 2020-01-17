1ae89f49e15c769afc0e53800845457de5aca9b5
Daniel Schmidt <horazius@outlook.com>
2020-01-17 05:56:34 +0100

    customized config

--- config.js
+++ config.js
@@ -1,17 +1,16 @@
 module.exports = {
-  title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
-  description: 'Full-stack Web Developer',
-  primaryColor: '#3498db', // Required
-  showHeaderImage: true,
-  showShareButtons: true,
-  postsPerPage: 5, // Required
-  social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
-  },
-  pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+    title: "Dev Blog", // Required
+    author: "Daniel Schmidt", // Required
+    description: "Full-stack Web Developer",
+    primaryColor: "#3498db", // Required
+    showHeaderImage: true,
+    showShareButtons: true,
+    postsPerPage: 5, // Required
+    social: {
+        website: "https://horazius.netlify.com",
+        github: "https://github.com/horazius77",
+        linkedin: "https://www.linkedin.com/in/daniel-schmidt-403b2935/",
+    },
+    pathPrefix: "/",
+    siteUrl: "https://horazius.netlify.com",
 };
